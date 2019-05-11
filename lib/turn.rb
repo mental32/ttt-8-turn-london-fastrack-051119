d83f28f418cf6f396fdfd307a9f19b5e1e6c0e2d
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-11 12:46:19 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,39 @@
+def position_taken?(board, index)
+  index && !([" ", "", nil].include? board[index])
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(s)
+  s.to_i - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+  board
+end
+
+def turn(board)
+  loop do
+    puts "Please enter 1-9:"
+    index = input_to_index gets.strip
+  
+    if valid_move?(board, index)
+      move(board, index)
+      display_board board
+      break
+    end
+  end
+end
